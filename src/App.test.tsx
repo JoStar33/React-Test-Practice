b65e027f295e7bb6b85f9981ec4b9c76d5bc4a7a
HoCG <rhwkd5012@naver.com>
2023-02-26 14:04:19 +0900

    feat: 기본 테스트 진행

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,16 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
+  //앱 렌더후
   render(<App />);
+  //스크린 객체를 통해 DOM 요소에 접근 가능.
   const linkElement = screen.getByText(/learn react/i);
+  //linkElement가 Document안에 있는지 검사.
   expect(linkElement).toBeInTheDocument();
 });
+
+test("로고 이미지가 잘 나온다.", () => {
+  render(<App/>);
+  const logoEl = screen.getByAltText("logo");
+  expect(logoEl).toBeInTheDocument();
+})
