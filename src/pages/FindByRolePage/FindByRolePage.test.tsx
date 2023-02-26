eb68319e96228268b47cfeb8de196b9494ac34a8
HoCG <rhwkd5012@naver.com>
2023-02-26 15:04:17 +0900

    feat: 늦게보이는 페이지 대응방법

---
+++ src/pages/FindByRolePage/FindByRolePage.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react";
+import FindByRolePage from "./FindByRolePage";
+
+
+test("늦게보이는 페이지에 대해 대응하는 방법을 알아보죠.", async () => {
+  render(<FindByRolePage/>)
+  const titleEl = await screen.findByRole("heading", {
+    name: "이제 보입니다."
+  },{
+    timeout: 2000,
+  });
+  expect(titleEl).toBeInTheDocument();
+})
---
+++ src/pages/FindByRolePage/FindByRolePage.tsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react';
+
+const FindByRolePage = () => {
+  const [titleShow, setTitleShow] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setTitleShow(!titleShow);
+    }, 1500);
+  })
+  return (
+    <div>
+      {
+        titleShow && <h1>이제 보입니다.</h1>
+      }
+    </div>
+  );
+};
+
+export default FindByRolePage;
